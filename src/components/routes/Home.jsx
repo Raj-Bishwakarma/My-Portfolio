c502554ead0ad6fbb8132cc97148c8850553c496
Raj-Bishwakarma <rankpusher086@gmail.com>
2024-09-07 13:58:30 +0545

    second commit

--- src/components/routes/Home.jsx
+++ src/components/routes/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div className='flex flex-col'>
         <div className='text-4xl my-5 '>Hello, I'm Raj Bishwakarma.</div>
         <hr className="h-px my-2 bg-gray-200 border-0"></hr>
-        <div className='text-2xl my-5'>Welcome to my Portfolio! I’m new in Web Development and I'm looking forward on this career.</div>
+        <div className='text-2xl my-5'>Welcome to my Portfolio! I’m new in Web Development and I'm looking forward.</div>
         {/* <Link to="/biodata" className="download-button hover:underline">Download My Bio Data</Link> */}
     </div>
   )
